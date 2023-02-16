a39663ebc1324b65450c80316f3243504136c5ae
Fezeka <bronwynmakhetha@gmail.com>
2023-02-16 02:40:47 -0800

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - prints "with proper grammar, but the outcome is
+ * main - Prints "with proper grammar, but the outcome is
  * a piece of art," followed by a newline
  * Return: Always 0.
  *
