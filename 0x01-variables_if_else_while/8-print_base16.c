5dabd76c1d099f534e2688b0c0cbd49b4f05fd55
Fezeka <bronwynmakhetha@gmail.com>
2023-02-18 00:40:58 -0800

    8-print_base16

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -4,21 +4,27 @@
 
 #include <time.h>
 
-/** 
+/**
  * main - Entry point
  * Return: Always 0 (Success)
  *
  */
 
+
 int main(void)
 
 {
+	int d;
 
-	char low;
-
-	for (low = 'z'; low >= 'a'; low--)
-	putchar(low);
+	for (d = '0'; d <= '9'; d++)
+	{
+	putchar(d);
+	if (d != '9')
+	{
+	putchar(',');
+	putchar(' ');
+	}
+	}
 	putchar('\n');
-
 	return (0);
 }
