63aa873efda33a178b0daa1c6d5867a5543882a0
Fezeka <bronwynmakhetha@gmail.com>
2023-03-08 08:18:07 -0800

    0-puts_recursion

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * factorial - Factorial of a given number
+ * @n: pointer block of memory to fill
+ * Return: factorial
+ */
+
+int factorial(int n)
+{
+	int result =n;
+
+	if (n < 0)
+		return (-1);
+
+	else if (n >= 0 && n <= 1)
+		return (1);
+}
