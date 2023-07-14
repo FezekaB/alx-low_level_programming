325288c4f257f754a2f49b1973d6c7080a477a81
Fezeka Makhetha <bronwynmakhetha@gmail.com>
2023-07-14 09:29:56 -0700

    0-malloc_checked

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -7,20 +7,15 @@
  *
  * Return: a pointer to the allocated memory
  */
-
 void *malloc_checked(unsigned int b)
-
 {
 	void *ptr;
 
-
 	ptr = malloc(b);
 
-
 	if (ptr == NULL)
-
 		exit(98);
-
 
 	return (ptr);
 }
+
